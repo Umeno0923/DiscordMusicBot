0050b119ed184063d103cd7ef74448abba433761
Umeno0923 <ihsasum0923@icloud.com>
2023-12-18 23:24:38 +0900

    v1.0.0

---
+++ src/main/java/com/relaxingleg/Main.java
@@ -0,0 +1,11 @@
+package com.relaxingleg;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JDA jda = JDABuilder.createDefault("").build();
+    }
+}
