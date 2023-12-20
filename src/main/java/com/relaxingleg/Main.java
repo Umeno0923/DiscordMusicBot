59f36d1d377bf1d534e63bb2dc57891fbd8ead98
Umeno0923 <ihsasum0923@icloud.com>
2023-12-20 23:25:28 +0900

    変更

--- src/main/java/com/relaxingleg/Main.java
+++ src/main/java/com/relaxingleg/Main.java
@@ -4,8 +4,7 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 
 public class Main {
-
     public static void main(String[] args) {
-        JDA jda = JDABuilder.createDefault("").build();
+        JDA jda = JDABuilder.createDefault("MTE4NjMxMDY4OTA2NTgxNjE5Ng.G3Vylc.gxo-a89CAJqUUm3TYhJc9zDwy2dYcElYSWcGwg").build();
     }
 }
